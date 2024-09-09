01a69a42aae53e8c9212375e30094c36c0b54582
Lucas Lomiento <lucaslomiento@outlook.com>
2024-09-09 14:25:09 -0300

    adds `/api/v1/migraitons` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,9 +5,9 @@ test("GET to /api/v1/status should return 200", async () => {
   const responseBody = await response.json();
 
   const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
-  expect(responseBody.update_at).toEqual(parsedUpdatedAt)
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0")
-  expect(responseBody.dependencies.database.max_connections).toEqual(100)
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1)
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
